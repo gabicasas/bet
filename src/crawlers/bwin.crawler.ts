3d4dfcd334e5ab69db5d00c63e5b9bd279a23615
Gabi <gabicasas@gmail.com>
2019-05-07 23:06:45 +0200

    Obtiene la info de mutation observer

--- src/crawlers/bwin.crawler.ts
+++ src/crawlers/bwin.crawler.ts
@@ -1,5 +1,5 @@
-import {URL} from 'url';
-import {mkdirSync, existsSync} from 'fs';
+import { URL } from 'url';
+import { mkdirSync, existsSync } from 'fs';
 import * as puppeteer from 'puppeteer';
 
 
@@ -12,24 +12,26 @@ export class BwinCrawler {
   constructor(baseUrl: string) {
     this.baseUrl = baseUrl;
   }
- 
+
   crawl(site: any) {
     (async () => {
       // Wait for browser launching.
-     const browser = await puppeteer.launch({devtools:true,
-      headless:false,
-      args: [
-        '--disable-extensions-except=C:\\Users\\Gabi\\eclipse2-workspace\\chrome_extension'
-      ],
-      executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'});
-     // Wait for creating the new page.
-     const page = await browser.newPage();
+      const browser = await puppeteer.launch({
+        devtools: true,
+        headless: false,
+        args: [
+          '--disable-extensions-except=C:\\Users\\Gabi\\eclipse2-workspace\\chrome_extension'
+        ],
+        executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+      });
+      // Wait for creating the new page.
+      const page = await browser.newPage();
 
-     
-  
-     await this.crawlInternal(page, `${this.baseUrl}`, site['children'], site['name']);
-  
-     browser.close();
+
+
+      await this.crawlInternal(page, `${this.baseUrl}`, site['children'], site['name']);
+
+      browser.close();
     })();
   }
 
@@ -44,13 +46,15 @@ export class BwinCrawler {
 
     await clientCDP.send('Network.enable');
 
-    await clientCDP.send('Network.setRequestInterception',{ patterns: [
-      { 
-        urlPattern: '*', 
-        resourceType: 'Script', 
-        interceptionStage: 'HeadersReceived' 
-      }
-    ]});
+    await clientCDP.send('Network.setRequestInterception', {
+      patterns: [
+        {
+          urlPattern: '*',
+          resourceType: 'Script',
+          interceptionStage: 'HeadersReceived'
+        }
+      ]
+    });
 
 
     /**
@@ -59,11 +63,11 @@ export class BwinCrawler {
      */
     clientCDP.on('Network.requestIntercepted', async ({ interceptionId, request, responseHeaders, resourceType }) => {
       console.log(`Intercepted ${request.url} {interception id: ${interceptionId}}`);
-  
-      const response = await clientCDP.send('Network.getResponseBodyForInterception',{ interceptionId });
-  
-     
-  
+
+      const response = await clientCDP.send('Network.getResponseBodyForInterception', { interceptionId });
+
+
+
       console.log(`Continuing interception ${interceptionId}`)
       clientCDP.send('Network.continueInterceptedRequest', {
         interceptionId,
@@ -72,30 +76,31 @@ export class BwinCrawler {
     });
 
     // https://stackoverflow.com/questions/48375700/how-to-use-puppeteer-to-dump-websocket-data
-    clientCDP.on('Network.webSocketCreated', ({requestId, url}) => {
-      console.log('Network.webSocketCreated', requestId, url)
-    })
-  
-    clientCDP.on('Network.webSocketClosed', ({requestId, timestamp}) => {
-      console.log('Network.webSocketClosed', requestId, timestamp)
-    })
-  
-    clientCDP.on('Network.webSocketFrameSent', ({requestId, timestamp, response}) => {
-      console.log('Network.webSocketFrameSent', requestId, timestamp, response.payloadData)
-    })
-    clientCDP.on('Network.webSocketFrameReceived', ({requestId, timestamp, response}) => {
-      console.log('Network.webSocketFrameReceived', requestId, timestamp, response.payloadData)
-    })
-
-   //https://medium.com/@jsoverson/using-chrome-devtools-protocol-with-puppeteer-737a1300bac0
+    /*
+      clientCDP.on('Network.webSocketCreated', ({requestId, url}) => {
+        console.log('Network.webSocketCreated', requestId, url)
+      })
+    
+      clientCDP.on('Network.webSocketClosed', ({requestId, timestamp}) => {
+        console.log('Network.webSocketClosed', requestId, timestamp)
+      })
+    
+      clientCDP.on('Network.webSocketFrameSent', ({requestId, timestamp, response}) => {
+        console.log('Network.webSocketFrameSent', requestId, timestamp, response.payloadData)
+      })
+      clientCDP.on('Network.webSocketFrameReceived', ({requestId, timestamp, response}) => {
+        console.log('Network.webSocketFrameReceived', requestId, timestamp, response.payloadData)
+      })
+  */
+    //https://medium.com/@jsoverson/using-chrome-devtools-protocol-with-puppeteer-737a1300bac0
 
 
     // Escucho algunos consoles que lanzo yo en el mutation observer
-   page.on('console', async (msg) => {
-    if (msg._text === '__mutation') {
-      console.log('CAPURTA EL CONSOLE, Hay QUE HACER QUE LOS MUTATATION OBSERVER LLAMER AL CONSOLE');
-    }
- }) 
+    page.on('console', async (msg) => {
+      if (msg._text.indexOf('mutObs:')==0) {
+        console.log(msg._text);
+      }
+    })
 
     // Create a directory storing the result PDFs.
     if (!existsSync(dirname)) {
@@ -104,8 +109,8 @@ export class BwinCrawler {
 
     // Go to the target page.
     let url = new URL(path);
-    await page.goto(path, {waitUntil: 'networkidle2'});
-   
+    await page.goto(path, { waitUntil: 'networkidle2' });
+
     debugger;
     // Take a snapshot in PDF format.
     /*await page.pdf({path: 
@@ -113,60 +118,60 @@ export class BwinCrawler {
     if (selectors.length == 0) {
       return;
     }
-  
-    let vari = 2;
+
+
     // #imsg-cont
-    let a=await page.evaluate(()=> {
-      debugger;
-      let a=document.querySelectorAll('#scoreboard > div.content > div > lbk-scoreboard-details > div > div > div > table > tbody > tr > td:nth-child(2) > span > span.counter-number.divider');
-      let b=null;
+    let a = await page.evaluate(() => {
+   
+      let a = document.querySelectorAll('#scoreboard > div.content > div > lbk-scoreboard-details > div > div > div > table > tbody > tr > td:nth-child(2) > span > span.counter-number.divider');
+      let b = null;
 
 
       // Pongo un mutation observer observando el elemento que tiene la cuota y logueo cada vez que cambia para que lo obtenga puppeteer 
 
-      const observer = new MutationObserver(
-        function() {
-          // communicate with node through console.log method
-          console.log('__mutation')
-         }
-        )
-        const config = {
-          attributes: true,
-          childList: true,
-          characterData: true,
-          subtree: true
-        }
-        observer.observe(a[0], config);
-     })
+      const observer = new MutationObserver(function (mutations) {
+        debugger;
+        mutations.forEach(function (mutation) {
+          console.log('mutObs:'+mutation.target['innerHTML']);
+        });
+      });
+      const config = {
+        attributes: true,
+        childList: true,
+        characterData: true,
+        subtree: true
+      }
+      observer.observe(a[0], config);
+      //})
 
 
 
 
-      vari=5;
+
       return a[0].innerHTML;
     })
 
-    
+
 
     debugger;
     console.log(a);
-       // Traversing in an order of BFS.
+    // Traversing in an order of BFS.
     let items: string[] = await page.evaluate((sel) => {
       let ret = [];
-       for (let item of document.querySelectorAll(sel)) {
-         console.log(item);
+      for (let item of document.querySelectorAll(sel)) {
+        console.log(item);
         let href = item.getAttribute('href');
         ret.push(href);
-       }
-       return ret;
+      }
+      return ret;
     }, selectors[0]);
-  
+
     for (let item of items) {
       console.log(`Capturing ${item}`);
-      await this.crawlInternal(page, 
+      await this.crawlInternal(page,
         `${item}`, selectors[0]['children'], `${dirname}/${selectors[0]['name']}`)
     }
   }
 }
 
-new BwinCrawler('https://livebetting.bwin.es/es/live?trid=in10937#/8422464').crawl({name:'bwin',children:['#scoreboard > div.content > div > lbk-scoreboard-details > div > div > div > table > tbody > tr > td:nth-child(2) > span > span.counter-number.divider']})
+new BwinCrawler('https://livebetting.bwin.es/es/live?trid=in10937#/8422464').crawl({ name: 'bwin', children: ['#scoreboard > div.content > div > lbk-scoreboard-details > div > div > div > table > tbody > tr > td:nth-child(2) > span > span.counter-number.divider'] })
